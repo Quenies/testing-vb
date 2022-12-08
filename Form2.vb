c3296f96d442e2c40ebb99666db539454a65ffd5
Quenies <kazuraabiyah@gmail.com>
2022-12-08 14:33:45 +0700

    Menambahkan form2

--- Form2.vb
+++ Form2.vb
@@ -1,6 +1,6 @@
-﻿Public Class Form1
+﻿Public Class Form2
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Form2.Show()
-        Me.Hide()
+        Form1.Show()
+        Me.Close()
     End Sub
 End Class
