d38c199f354feb2476b3f28cc206427a2aa54c6c
Queniex <queniesalbiyah@gmail.com>
2022-12-08 14:45:13 +0700

    Membuat form3

--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,9 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        form3.Show()
+        Me.Hide()
+    End Sub
 End Class
